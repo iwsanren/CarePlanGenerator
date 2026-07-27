6e3bc317e07f32589ec57189117628e8376e7121
Zhaofang <h1020230042@qq.com>
2026-07-27 23:02:39 +0100

    Infra: initiate terraform

---
+++ backend/infra/s3-test/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+  profile = "careplan-dev"
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket        = "my-test-bucket-12345"
+  force_destroy = true
+
+  tags = {
+    Project = "careplan-generator"
+    Purpose = "terraform-day15-test"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "test_bucket" {
+  bucket = aws_s3_bucket.test_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
